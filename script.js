b9c6173fa6bf9d4271a68af302cabc9ca481b155
Gavin Quirk <gavinquirk@gmail.com>
2021-09-20 17:26:07 -0700

    inserting list items into dom

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ const movies = [
   'Arrival',
   'Gravity',
 ];
+
+// Store list items
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// Insert list items into DOM
+function createList() {
+  [...movies].forEach((movie, index) => {
+    const listItem = document.createElement('li');
+
+    listItem.setAttribute('data-index', index);
+
+    listItem.innerHTML = `
+      <span class="number">${index + 1}</span>
+      <div class="draggable" draggable="true">
+        <p class="movie-name">${movie}</p>
+        <i class="fas fa-grip-lines"></i>
+      </div>
+    `;
+
+    listItems.push(listItem);
+
+    draggable_list.appendChild(listItem);
+  });
+}
