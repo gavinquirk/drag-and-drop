5e5b0fffd333f555d4742fa531da38db73a8375f
Gavin Quirk <gavinquirk@gmail.com>
2021-09-20 17:25:44 -0700

    html elements in js, list of movie names

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('working...');
+
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const movies = [
+  'Star Wars: A New Hope',
+  'Alien',
+  'Aliens',
+  'Close Encounters of the Third Kind',
+  'Blade Runner',
+  'Gattaca',
+  'A.I. Artificial Intelligence',
+  'Mad Max: Fury Road',
+  'Arrival',
+  'Gravity',
+];
